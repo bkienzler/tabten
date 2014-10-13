e8fd97186a80c644f47778f6c4a4a06be63040e0
bkienzler <mail@benedikt-kienzler.com>
2014-10-13 13:39:26 +0000

    Updated usermodel

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,10 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-<<<<<<< HEAD
-  :recoverable, :rememberable, :trackable, :validatable
-  validates :ranking, uniqueness: true
-
-=======
          :recoverable, :rememberable, :trackable, :validatable
   validates :ranking, :name, uniqueness: true
   validates :ranking, :name, :email, presence: true
   
->>>>>>> 08266ed112900d40a948a94debb8ce43b34d0a09
   include UserUtil
 
   def level
