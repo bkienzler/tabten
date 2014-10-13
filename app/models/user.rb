ccc9f44280c2fc5ede95bc0e4664e373c455981a
bkienzler <mail@benedikt-kienzler.com>
2014-10-13 12:56:34 +0000

    Change on usermodel

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,15 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
   validates :ranking, uniqueness: true
-  
+
   include UserUtil
-  
+
   def level
     triangle_numbers(self.ranking).size
   end 
-  
+
   def challenge_range
     Array(self.ranking - (self.level - 1) .. self.ranking - 1)
   end
